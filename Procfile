bdf1deac07ef6eecdfcc544fec918368f5008dd6
Justine Biju Paul <justinebijupaul@gmail.com>
2024-10-06 22:28:29 +0530

    update gunicorn

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn app:app
+web: gunicorn -c gunicorn.conf.py app:app
 web: gunicorn --workers 3 --bind 0.0.0.0:$PORT app:app
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,3 @@
 # gunicorn.conf.py
-
-import multiprocessing
-
-bind = "0.0.0.0:8080"  # Bind to all IP addresses on port 8080
-workers = multiprocessing.cpu_count() * 2 + 1  # Number of worker processes
-threads = 2 
+bind = "0.0.0.0:8000"
+workers = 3
